00b9a5ecff7528088c336eb6fcc1f8fd583a8e9d
Lucas da Costa <aintluks@gmail.com>
2021-08-15 13:13:57 -0300

    del: validate, modified test helper

--- lib/person_validate.rb
+++
@@ -1,5 +0,0 @@
-class PersonValidate
-  def self.date_birthday(birthday)
-    birthday.nil? || birthday.empty? ? nil : Date.parse(birthday)
-  end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,6 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "contacts"
 require "person"
+require "iperson"
+require "csv_handler"
 require "minitest/autorun"
