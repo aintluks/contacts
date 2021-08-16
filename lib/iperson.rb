129c9d10f5f7432c34fae9c2f7e6711bb241854a
Lucas da Costa <aintluks@gmail.com>
2021-08-15 21:09:34 -0300

    refact: messages variable to Iperson

--- lib/iperson.rb
+++ lib/iperson.rb
@@ -3,10 +3,18 @@ require 'csv_handler'
 class IPerson
   def initialize
     @db = CsvHandler.new
+    @messages = {
+      :saved => "|Saved successfully!!",
+      :already_saved => "|Contact already saved!",
+      :not_found => "|Contact not found...",
+      :no_contacts => "|No contacts to show...",
+      :deleted => "|Deleted successfully!!",
+      :updated => "|Updated successfully!"
+    }
   end
 
   def save(person)
-    find(person.name) ? "|Contact already saved!" : @db.append(person.to_array) && "|Saved successfully!!"
+    find(person.name) ? @messages[:already_saved] : @db.append(person.to_array) && @messages[:saved]
   end
 
   def find(name)
@@ -16,21 +24,21 @@ class IPerson
 
   def list_all
     contacts = @db.read[1..]
-    contacts.empty? ? "|No contacts to show..." : display(contacts)
+    contacts.empty? ? @messages[:no_contacts] : display(contacts)
   end
 
   def list_one(name)
     person = find(name) || []
-    person.empty? ? "|Contact not found..." : display([person])
+    person.empty? ? @messages[:not_found] : display([person])
   end
 
   def delete(name)
     person = @db.delete(name)
-    person.empty? ? "|Contact not found..." : "|Deleted successfully!!"
+    person.empty? ? @messages[:not_found] : @messages[:deleted]
   end
 
   def update(new_data)
-    @db.update(new_data) && "|Updated successfully!" 
+    @db.update(new_data) && @messages[:updated]
   end
 
   private
